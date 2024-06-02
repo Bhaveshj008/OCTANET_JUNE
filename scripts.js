d3d9d924fe29000edddf6187bc033d2b8bde2831
bhavesh008 <bhavesh002jadhav@gmail.com>
2024-06-02 18:58:15 +0530

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Toggle Navbar on Button Click
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    document.getElementById('navbarNav').classList.toggle('show');
+});
